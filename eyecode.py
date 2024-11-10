50924d5a6a7e9b139305188501dcb3760da19961
unknown <minhaz.ahmed000@gmail.com>
2024-11-10 11:45:20 +0530

    eye color extraction added

--- eyecode.py
+++ eyecode.py
@@ -1,50 +1,37 @@
-import numpy as np
 import cv2
-from mtcnn.mtcnn import MTCNN
+import numpy as np
+from collections import Counter
+import face_recognition  
 
-detector = MTCNN()
+def detect_iris_color(image_path):
+    image = cv2.imread(image_path)
+    
+    face_landmarks_list = face_recognition.face_landmarks(image)
+    
+    if not face_landmarks_list:
+        return "No face detected"
+    
+    for face_landmarks in face_landmarks_list:
+        left_eye = np.array(face_landmarks['left_eye'])
+        right_eye = np.array(face_landmarks['right_eye'])
+        
+        for eye in [left_eye, right_eye]:
+            mask = np.zeros(image.shape[:2], dtype=np.uint8)
+            cv2.fillPoly(mask, [eye], 255)
+            
+            eye_region = cv2.bitwise_and(image, image, mask=mask)
+            
+            rgb = cv2.cvtColor(eye_region, cv2.COLOR_BGR2RGB)
+            
+            flat_pixels = [tuple(pixel) for line in rgb for pixel in line]
+            color_counts = Counter(flat_pixels)
+            top_colors = color_counts.most_common(5)
+            if top_colors[0][0] == (0,0,0):
+                return top_colors[1][0]
+            else:
+                return top_colors[0][0]
+    
+    return "Could not determine iris color"
 
-def eye_color(image):
-    color_dict = {}
-    hue_list = []
-
-    img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    h, w = image.shape[0:2]
-    imgMask = np.zeros((image.shape[0], image.shape[1], 1))
-
-    result = detector.detect_faces(image)
-    if result == []:
-        print('Warning: Can not detect any face in the input image!')
-        return
-
-    left_eye = result[0]['keypoints']['left_eye']
-    right_eye = result[0]['keypoints']['right_eye']
-
-    eye_distance = np.linalg.norm(np.array(left_eye)-np.array(right_eye))
-    eye_radius = eye_distance/11
-
-    cv2.circle(imgMask, left_eye, int(eye_radius), (255,255,255), -1)
-    cv2.circle(imgMask, right_eye, int(eye_radius), (255,255,255), -1)
-
-    cv2.circle(image, left_eye, int(eye_radius), (0, 155, 255), 1)
-    cv2.circle(image, right_eye, int(eye_radius), (0, 155, 255), 1)
-
-    # cv2.imshow('Eyes Detected', image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-    for y in range(h):
-        for x in range(w):
-            if imgMask[y, x] != 0:
-                color = tuple(map(int, img[y, x]))
-                hue_list.append(img[y,x][0])
-                if color in color_dict:
-                    color_dict[color] += 1
-                else:
-                    color_dict[color] = 1
-
-    avg_hue = sum(hue_list) / len(hue_list)
-    return avg_hue
-
-image = cv2.imread('image.jpg')
-print(eye_color(image))
+result = detect_iris_color("image.jpg")
+print(result)
--- skincode.py
+++ skincode.py
@@ -2,25 +2,29 @@ import cv2
 import numpy as np
 from collections import Counter
 
-# detecting skin pixels
-min_YCrCb = np.array([0,133,77],np.uint8)
-max_YCrCb = np.array([235,173,127],np.uint8)
+def pixel_dict(x):
+    # detecting skin pixels
+    min_YCrCb = np.array([0,133,77],np.uint8)
+    max_YCrCb = np.array([235,173,127],np.uint8)
 
-image = cv2.imread("image.jpg")
-imageYCrCb = cv2.cvtColor(image,cv2.COLOR_BGR2YCR_CB)
-skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
+    image = cv2.imread(x)
 
-skinYCrCb = cv2.bitwise_and(image, image, mask = skinRegionYCrCb)
+    imageYCrCb = cv2.cvtColor(image,cv2.COLOR_BGR2YCR_CB)
+    skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
+    skinYCrCb = cv2.bitwise_and(image, image, mask = skinRegionYCrCb)
 
-# major colourcode
-def pixel_dict(image):
-    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    # major colourcode
+    image = cv2.cvtColor(skinYCrCb,cv2.COLOR_BGR2RGB)
     flat_pixels = [tuple(pixel) for line in image for pixel in line]
     color_counts = Counter(flat_pixels)
-    top_colors = color_counts.most_common(5)
-    return top_colors
+    top_colors = color_counts.most_common(2)
+    if top_colors[0][0] == (0,0,0):
+        return top_colors[1][0]
+    else:
+        return top_colors[0][0]
 
-print(pixel_dict(skinYCrCb))
+
+print(pixel_dict("image.jpg")) #image location
 
 
 
